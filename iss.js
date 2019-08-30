b142acb2e50d1f7bf533d00c68d074dc07cb4e5a
ChrisnNg <cristopherng@hotmail.com>
2019-08-30 04:52:11 +0000

    ISS completed and eslinted

--- iss.js
+++ iss.js
@@ -1,12 +1,5 @@
 const request = require('request');
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
+
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org/?format=json', (error, response, body) => {
@@ -56,15 +49,7 @@ const fetchISSFlyOverTimes = function(coords, callback) {
   });
 };
 
-/**
- * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
- * Input:
- *   - A callback with an error or results. 
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The fly-over times as an array (null if error):
- *     [ { risetime: <number>, duration: <number> }, ... ]
- */ 
+
 const nextISSTimesForMyLocation = function(callback) {
 
   fetchMyIP((error, ip) => {
